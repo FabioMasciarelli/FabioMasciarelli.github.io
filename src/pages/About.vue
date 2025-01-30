ae16f1e43340a633f99a83e25c1ec8d948829e09
Fabio Masciarelli <158151713+FabioMasciarelli@users.noreply.github.com>
2025-01-30 16:40:18 +0100

    add: description at icons

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -17,37 +17,112 @@
             <hr>
             <h2>CODE</h2>
         </div>
-        <ul class="code">
-            <li>
+
+        <!-- From Uiverse.io by david-mohseni -->
+        <ul class="wrapper code">
+            <li class="icon code">
+                <span class="tooltip">HTML5</span>
                 <i class="fa-brands fa-html5"></i>
             </li>
-            <li>
+            <li class="icon code">
+                <span class="tooltip">CSS3</span>
                 <i class="fa-brands fa-css3-alt"></i>
             </li>
-            <li>
+            <li class="icon code">
+                <span class="tooltip">Bootstrap</span>
                 <i class="fa-brands fa-bootstrap"></i>
             </li>
-            <li>
+            <li class="icon code">
+                <span class="tooltip">JavaScript</span>
                 <i class="fa-brands fa-js"></i>
             </li>
-            <li>
+            <li class="icon code">
+                <span class="tooltip">Vue.js</span>
                 <i class="fa-brands fa-vuejs"></i>
             </li>
-            <li>
+            <li class="icon code">
+                <span class="tooltip">PHP</span>
                 <i class="fa-brands fa-php"></i>
             </li>
-            <li>
+            <li class="icon code">
+                <span class="tooltip">Laravel</span>
                 <i class="fa-brands fa-laravel"></i>
             </li>
-            <li>
+            <li class="icon code">
+                <span class="tooltip">MySQL</span>
                 <i class="fa-solid fa-database"></i>
             </li>
         </ul>
+
     </section>
 </template>
 
 <style scoped lang="scss">
 @use "../style/partials/variables" as *;
+
+/* From Uiverse.io by david-mohseni */
+.wrapper {
+    display: inline-flex;
+    list-style: none;
+    width: 100%;
+    padding-top: 40px;
+    justify-content: center;
+}
+
+.wrapper .icon {
+    position: relative;
+    background-color: rgba(255, 255, 255, 0.3);
+    border-radius: 50%;
+    margin: 10px;
+    width: 100px;
+    height: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    cursor: pointer;
+}
+
+.wrapper .tooltip {
+    position: absolute;
+    top: 0;
+    padding: 5px 8px;
+    border-radius: 5px;
+    opacity: 0;
+    pointer-events: none;
+    transition: all 0.3s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+}
+
+.wrapper .tooltip::before {
+    position: absolute;
+    content: "";
+    height: 8px;
+    width: 8px;
+    background: #fff;
+    bottom: -3px;
+    left: 50%;
+    transform: translate(-50%) rotate(45deg);
+    transition: all 0.3s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+}
+
+.wrapper .icon:hover .tooltip {
+    top: -45px;
+    opacity: 1;
+    visibility: visible;
+    pointer-events: auto;
+}
+
+.wrapper .icon:hover span,
+.wrapper .icon:hover .tooltip {
+    text-shadow: 0px -1px 0px rgba(0, 0, 0, 0.1);
+}
+
+.wrapper .code:hover,
+.wrapper .code:hover .tooltip,
+.wrapper .code:hover .tooltip::before {
+    background: $orange;
+}
+
 
 section {
     width: 100%;
@@ -59,6 +134,7 @@ section {
 
     .description {
         margin-bottom: 100px;
+
         p {
             a {
                 color: $orange;
--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -11,17 +11,90 @@
             <h2>CONTACTS</h2>
         </div>
         <h3>Let's stay in touch!</h3>
-        <ul class="contacts">
-            <li><a href="https://www.linkedin.com/in/fabio-masciarelli-1605b7304/"><i
-                        class="fa-brands fa-linkedin"></i></a></li>
-            <li><a href="https://github.com/FabioMasciarelli"><i class="fa-brands fa-square-github"></i></a></li>
-            <li><a href="mailto:fabiomasciarelliwd@gmail.com"><i class="fa-solid fa-envelope"></i></a></li>
+        <!-- From Uiverse.io by david-mohseni -->
+        <ul class="wrapper contacts">
+            <li class="icon contact">
+                <span class="tooltip">Linkedin</span>
+                <a href="https://www.linkedin.com/in/fabio-masciarelli-1605b7304/"><i
+                        class="fa-brands fa-linkedin"></i></a>
+            </li>
+            <li class="icon contact">
+                <span class="tooltip">Github</span>
+                <a href="https://github.com/FabioMasciarelli"><i class="fa-brands fa-square-github"></i></a>
+            </li>
+            <li class="icon contact">
+                <span class="tooltip">Email</span>
+                <a href="mailto:fabiomasciarelliwd@gmail.com"><i class="fa-solid fa-envelope"></i></a>
+            </li>
         </ul>
     </section>
 </template>
 
 <style scoped lang="scss">
 @use "../style/partials/variables" as *;
+
+.wrapper {
+    display: inline-flex;
+    list-style: none;
+    width: 100%;
+    padding-top: 40px;
+    justify-content: center;
+}
+
+.wrapper .icon {
+    position: relative;
+    background-color: rgba(255, 255, 255, 0.3);
+    border-radius: 50%;
+    margin: 10px;
+    width: 100px;
+    height: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    cursor: pointer;
+}
+
+.wrapper .tooltip {
+    position: absolute;
+    top: 0;
+    padding: 5px 8px;
+    border-radius: 5px;
+    opacity: 0;
+    pointer-events: none;
+    transition: all 0.3s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+}
+
+.wrapper .tooltip::before {
+    position: absolute;
+    content: "";
+    height: 8px;
+    width: 8px;
+    background: #fff;
+    bottom: -3px;
+    left: 50%;
+    transform: translate(-50%) rotate(45deg);
+    transition: all 0.3s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+}
+
+.wrapper .icon:hover .tooltip {
+    top: -45px;
+    opacity: 1;
+    visibility: visible;
+    pointer-events: auto;
+}
+
+.wrapper .icon:hover span,
+.wrapper .icon:hover .tooltip {
+    text-shadow: 0px -1px 0px rgba(0, 0, 0, 0.1);
+}
+
+.wrapper .contact:hover,
+.wrapper .contact:hover .tooltip,
+.wrapper .contact:hover .tooltip::before {
+    background: $orange;
+}
+
 
 section {
     width: 100%;
