7578bd2e7d65e64ac5926bbef254f4729cb7a55e
Fabio Masciarelli <158151713+FabioMasciarelli@users.noreply.github.com>
2025-02-11 10:41:31 +0100

    create Cards component and fill in Projects page

---
+++ src/components/Cards.vue
@@ -0,0 +1,55 @@
+<script>
+
+
+</script>
+
+
+<template>
+    <li class="card">
+        <img src="../assets/img/projects/logisticSite.jpeg" alt="">
+        <a class="description" href="https://transportsite.netlify.app">Logistic Site</a>
+    </li>
+    <li class="card"></li>
+    <li class="card"></li>
+    <li class="card"></li>
+    <li class="card"></li>
+    <li class="card"></li>
+
+</template>
+
+<style lang="scss" scoped>
+@use "../style/partials/variables" as *;
+
+.card {
+    width: calc(100% / 3 - 40px);
+    background-color: $main;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    position: relative;
+
+    &:hover {
+        box-shadow: 0 0 6px 6px rgba(255, 255, 255, 0.6);
+        transition: all .6s ease-out;
+
+        .description {
+            display: block;
+        }
+    }
+
+    .description {
+        display: none;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 80px;
+        padding: 10px;
+        background-color: $main;
+        animation: slide-up .6s ease-out;
+        color: black;
+        text-decoration: none;
+        text-align: center;
+    }
+}
+</style>
--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,5 +1,11 @@
 <script>
+import Cards from '../components/Cards.vue';
 
+export default {
+    components: {
+        Cards
+    }
+}
 </script>
 
 <template>
@@ -9,15 +15,7 @@
             <h2>PROJECTS</h2>
         </div>
         <ul class="projects">
-            <li class="card">
-                <img src="../assets/img/fm.jpg" alt="">
-                <a class="description" href="https://transportsite.netlify.app">Transport Site</a>
-            </li>
-            <li class="card"></li>
-            <li class="card"></li>
-            <li class="card"></li>
-            <li class="card"></li>
-            <li class="card"></li>
+            <Cards />
         </ul>
     </section>
 </template>
@@ -59,39 +57,6 @@ section {
         flex-wrap: wrap;
         gap: 20px;
         list-style-type: none;
-
-        .card {
-            width: calc(100% / 3 - 40px);
-            background-color: $main;
-            display: flex;
-            flex-direction: column;
-            justify-content: space-around;
-            position: relative;
-
-            &:hover {
-                box-shadow: 0 0 6px 6px rgba(255, 255, 255, 0.6);
-                transition: all .6s ease-out;
-
-                .description {
-                    display: block;
-                }
-            }
-
-            .description {
-                display: none;
-                position: absolute;
-                bottom: 0;
-                left: 0;
-                width: 100%;
-                height: 80px;
-                padding: 10px;
-                background-color: $main;
-                animation: slide-up .6s ease-out;
-                color: black;
-                text-decoration: none;
-                text-align: center;
-            }
-        }
     }
 }
 </style>
